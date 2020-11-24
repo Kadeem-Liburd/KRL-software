e8bd18b574e4a2b2697715da5fcf06acc6bd301e
Kadeem-Liburd <k.liburd7@gmail.com>
2020-11-23 23:44:03 -0500

    Update Contact.php

--- Contact.php
+++ Contact.php
@@ -1,27 +1,49 @@
-<?php 
+<?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-$name = $_POST['name'];
-$vistor_email = $_POST['email'];
-$message = $_POST['message'];
+// Load Composer's autoloader
+require 'vendor/autoload.php';
 
-$email_from ='K.Liburd1993@gmail.com';
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
-$email_subject = "NEW FORM SUBMISSION";
+try {
+    //Server settings
+    $mail->SMTPDebug = 1;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp1.example.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'user@example.com';                     // SMTP username
+    $mail->Password   = 'secret';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-$email_body = "User name:$name.\n ."
-              " User Email: $visitor_email.\n".
-              " User Message: $message.\n";
+    //Recipients
+    $mail->setFrom('from@example.com', 'Mailer');
+    $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
+    $mail->addAddress('ellen@example.com');               // Name is optional
+    $mail->addReplyTo('info@example.com', 'Information');
+    $mail->addCC('cc@example.com');
+    $mail->addBCC('bcc@example.com');
 
+    // Attachments
+    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-$to= "K.Liburd7@gmail.com";
-
-$headers= "From: $email_from \r\n";
-
-$headers .="Reply-To: $visitor_email \r\n";
-
-mail ($to,$email_subject,$email_body,$headers);
-
-header("LocationL Contact.html"); 
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+} 
 
 ?>
